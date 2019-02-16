b399bce19a8b95ebd1cc6065ec0e0bea221fb169
olivier-boesch <47700716+olivier-boesch@users.noreply.github.com>
2019-02-16 22:09:09 +0100

    comments added on led.py

--- led.py
+++ led.py
@@ -3,20 +3,31 @@ from kivy.clock import Clock
 from kivy.properties import ListProperty, OptionProperty, BooleanProperty, NumericProperty, StringProperty
 
 class Led(Image):
+    """Led widget for kivy"""
+    # state of led
     state = OptionProperty("off", options=["on", "off"])
-    type = OptionProperty('color',options=['color','source','both'])
+    # led_type : what does change when state changes (color, source or both)
+    led_type = OptionProperty('color',options=['color','source','both'])
+    # color_on : color when state is on
     color_on = ListProperty([0.22,0.79,1,1])
+    # color_off : color when state is off
     color_off = ListProperty([0.05,0.175,0.225,1])
+    # source_on : image source when state is on
     source_on = StringProperty('')
+    # source_off : image source when state is off
     source_off = StringProperty('')
+    # auto_off : switch auto to off state when True
     auto_off=BooleanProperty(False)
+    # auto_off_delay : delay before switching off
     auto_off_delay=NumericProperty(0.2)
     
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        # force setting according to off state
         self.on_state(None,'off')
         
     def toggle_state(self):
+        """toggle_state : change state"""
         if self.state == 'on':
             self.state = 'off'
         else:
@@ -24,41 +35,43 @@ class Led(Image):
     
     def on_state(self,instance,state):
         if state=='on':
-            if self.type == 'color' or self.type == 'both':
+            if self.led_type == 'color' or self.led_type == 'both':
                 self.color=self.color_on
-            if self.type == 'source' or self.type == 'both':
+            if self.led_type == 'source' or self.led_type == 'both':
                 self.source=self.source_on
             if self.auto_off:
                 Clock.schedule_once(lambda dt: self.set_off(),self.auto_off_delay)
         else: # state==off
-            if self.type == 'source' or self.type == 'both':
+            if self.led_type == 'source' or self.led_type == 'both':
                 self.source=self.source_off
-            if self.type == 'color' or self.type == 'both':
+            if self.led_type == 'color' or self.led_type == 'both':
                 self.color=self.color_off
             
     def set_off(self):
         self.state='off'
     
     def on_source_on(self,instance,src):
-        if self.state == 'on' and (self.type == 'source' or self.type == 'both'):
+        if self.state == 'on' and (self.led_type == 'source' or self.led_type == 'both'):
             self.source=self.source_on
             
     def on_source_off(self,instance,src):
-        if self.state == 'off' and (self.type == 'source' or self.type == 'both'):
+        if self.state == 'off' and (self.led_type == 'source' or self.led_type == 'both'):
             self.source=self.source_off
             
     def on_color_on(self,instance,clr):
-        if self.state == 'on' and (self.type == 'color' or self.type == 'both'):
+        if self.state == 'on' and (self.led_type == 'color' or self.led_type == 'both'):
             self.color=self.color_on
             
     def on_color_off(self,instance,clr):
-        if self.state == 'off' and (self.type == 'color' or self.type == 'both'):
+        if self.state == 'off' and (self.led_type == 'color' or self.led_type == 'both'):
             self.color=self.color_off
             
-    def on_type(self, instance, tp):
+    def on_led_type(self, instance, tp):
         if tp == 'source':
+            # set color to white when led_type = 'source'
             self.color = [1,1,1,1]
             
+# example to demonstrate led widget capabilities
 if __name__ == '__main__':
     from kivy.app import App
     from kivy.clock import Clock
@@ -99,7 +112,7 @@ BoxLayout:
         BoxLayout:
             orientation: 'vertical'
             Label:
-                text: 'type=color (default)'
+                text: 'led_type=color (default)'
             Led:
                 id: led_typecolor
                 source: 'shapes/basic_disc.png'
@@ -108,19 +121,19 @@ BoxLayout:
         BoxLayout:
             orientation: 'vertical'
             Label:
-                text: 'type=source'
+                text: 'led_type=source'
             Led:
                 id: led_typesource
-                type: 'source'
+                led_type: 'source'
                 source_on: 'media/yes.png'
                 source_off: 'media/no.png'
         BoxLayout:
             orientation: 'vertical'
             Label:
-                text: 'type=both'
+                text: 'led_type=both'
             Led:
                 id: led_typeboth
-                type: 'both'
+                led_type: 'both'
                 source_on: 'shapes/basic_disc.png'
                 source_off: 'shapes/basic_square.png'
                 color_off: [1,0,0,1]
@@ -131,7 +144,7 @@ BoxLayout:
                 text: 'auto_off=True'
             Led:
                 id: led_autooff
-                type: 'color'
+                led_type: 'color'
                 source: 'shapes/basic_disc.png'
                 auto_off: True
         BoxLayout:
@@ -140,7 +153,7 @@ BoxLayout:
                 text: 'with animated image'
             Led:
                 id: led_animated
-                type: 'both'
+                led_type: 'both'
                 source_on: 'media/fan.zip'
                 source_off: 'media/fan.png'"""
         
